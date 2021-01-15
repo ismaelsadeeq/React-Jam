2125f55388cd2024c360b8fc2e9570316449b853
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2021-01-15 15:57:44 +0100

    stripe

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,31 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) =>{
+  
+  const [isSidebarOpen, setIsSidebarOpen]= useState(false);
+  const [isModalOpen,setIsModalOpen] = useState(false);
+
+  function openSidebar(){
+    setIsSidebarOpen(true)
+  }
+  function closeSidebar(){
+    setIsSidebarOpen(false)
+  }
+  function openModal(){
+    setIsModalOpen(true)
+  }
+  function closeModal(){
+    setIsModalOpen(false)
+  }
+  return <AppContext.Provider value={{
+    isModalOpen,isSidebarOpen,openModal,openSidebar,closeModal,closeSidebar
+  }}>{children }</AppContext.Provider>
+}
+//custom hook
+export const useGlobalContext = ()=>{
+  return useContext(AppContext)
+}
+
+export {AppContext, AppProvider }
